ac07f89203b4a4158ea4e52bc8954040ed02b6e6
FrankLazaroTIC <2023_frank.lazaro@iticbcn.cat>
2024-05-15 19:48:05 +0200

    FireBase instalat i conectat

---
+++ ProjecteFireBase/FireBase/src/components/config/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCK7tkviN2LRKdvnfRFW7xA7hDvH5c38IQ",
+    authDomain: "fir-frank-236ed.firebaseapp.com",
+    projectId: "fir-frank-236ed",
+    storageBucket: "fir-frank-236ed.appspot.com",
+    messagingSenderId: "573631824055",
+    appId: "1:573631824055:web:0bf9461c9e5af01423cd32"
+  };
+  
---
+++ ProjecteFireBase/FireBase/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { firebaseConfig } from './components/config/config.js' //Importem el access a la base de dades
+import * as firebase from 'firebase/app'; 
+import './index.css'
+
+firebase.initializeApp(firebaseConfig); //Inicialitzem la base de dades
+console.log(firebase.SDK_VERSION); //Comprovem la versió de la base de dades
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
